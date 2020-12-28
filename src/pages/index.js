73781531918ec056e763cc08d22f0e1242f7e329
twishtapyal11 <76672137+twishtapyal11@users.noreply.github.com>
2020-12-28 08:09:08 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,126 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section, StackItem, Stack } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Text margin="0px 0 24px 0" text-align="center" font="--headline2" md-font="--headline3">
+				BLACKBLUE
+			</Text>
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--base"
+				font-weight="700"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px" />
+				<Override slot="link-index">
+					October
+				</Override>
+				<Override slot="link-404">
+					1st
+				</Override>
+			</Menu>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Stack>
+				<StackItem width="50%" lg-width="100%" lg-order="2">
+					<Override slot="StackItemContent" text-align="center" />
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Jayeeta
+							</Text>
+							<Text color="--grey" margin="0">
+								Singer
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Jayani
+							</Text>
+							<Text color="--grey" margin="0">
+								{" "}Leader/ Singer
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Pratyush
+							</Text>
+							<Text color="--grey" margin="0">
+								DJ
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Twish{" "}
+							</Text>
+							<Text color="--grey" margin="0">
+								Music Producer
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" lg-margin-bottom="24px" flex-direction="column" />
+					<Text color="--grey" letter-spacing="1px" text-transform="uppercase" margin="0 0 10px 0">
+						Meet our team
+					</Text>
+					<Text as="h2" font="--headline2" md-font="--headline3" margin="0">
+						We Are Black Blue.
+						<br />
+						We are Indian Band
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				We are very good band and we love music very much
+			</Text>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe9400b94657a0021e7e1df",
+				"5fe9400b94657a0021e7e1e1"
+			]
+		},
+		"5fe9400b94657a0021e7e1df": {
+			"id": "5fe9400b94657a0021e7e1df",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe9400b94657a0021e7e1e1": {
+			"id": "5fe9400b94657a0021e7e1e1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
